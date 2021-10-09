b55270a842cb991f26d8fafd591bf548e99eadec
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-09 22:16:46 +0600

    конец урока 18

--- bin/lazy_dart_lessons.dart
+++ bin/lazy_dart_lessons.dart
@@ -1,3 +1,30 @@
-void main(List<String> arguments) {
-  print(555);
+import 'dart:convert';
+import 'dart:io';
+
+import 'dart:math';
+
+void main() {
+  final ed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  var beleberda = 0;
+  while (true) {
+    print('Ввeдите строку');
+    final string = stdin.readLineSync();
+    print('вы ввели: $string');
+    if (string == 'stop') {
+      break;
+    }
+  }
 }
+
+
+//  var beleber = 0;
+//   do {
+//     print('все норм но я стардаст');
+//     beleber = 1;
+//   } while (beleber < 1);
+
+//     beleberda = rin.nextInt(200);
+//   }
+//   print('все не очень хорошо');
+// }
