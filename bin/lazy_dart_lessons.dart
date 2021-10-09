f539af5e3ef1ce769de204ebdd0341da96073fa5
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-09 22:43:37 +0600

    урок 14

--- bin/lazy_dart_lessons.dart
+++ bin/lazy_dart_lessons.dart
@@ -1,51 +1,23 @@
-//import 'dart:html';
+void main(List<String> arguments) {
+  List<dynamic> mfkjj = [25, 08];
+  print(mfkjj);
+  mfkjj.add(2006);
+  mfkjj.add(2006);
+  mfkjj.add(2006);
+  //print(mfkjj);
+  //List<int> newListik = mfkjj.reversed.toList();
+  var newListik = mfkjj.reversed.toList();
+  // print(newListik);
+  // print(newListik.runtimeType);
+  // print(newListik.first);
+  // print(newListik.first.runtimeType);
 
-import 'dart:io';
-
-//import 'package:collection/collection.dart';
-
-void main() {
-  final DG = {
-    'ВЛАДДИМИР ВЛАИМИРОВИЧ',
-    // 'Лукошенко',
-    'Зеленский',
-    // 'Байдан',
-  };
-
-  final DGB = {
-    // 'ВЛАДДИМИР ВЛАИМИРОВИЧ',
-    // 'Лукошенко',
-    'Зеленский',
-    'Байдан',
-  };
-
-  final result = DG.union(DGB);
-  print(result);
-  var result2 = DG.difference(DGB);
-  print(result2);
+  // var tyty = newListik.first - 2006;
+  // print(tyty);
+  print(mfkjj);
+  //print(mfkjj.indexOf(777));
+  // var bbb = mfkjj.indexWhere((element) => element > 1000);
+  var sss = mfkjj.indexWhere((element) => element.isEven);
+  print('Подходит элемент номер $sss');
+  print(mfkjj[sss]);
 }
-
-
-
-
-
-
-
-
-  // final natsu = {
-  //   '123 1000': 'ВЛАДДИМИР ВЛАИМИРОВИЧ',
-  //   '123 1010': 'Лукошенко',
-  //   '123 1100': 'Зеленский',
-  //   '124 1000': 'Байдан',
-  // };
-  // final zenitsu = [
-  //   'ВЛАДДИМИР ВЛАИМИРОВИЧ',
-  //   'Лукошенко',
-  //   'Зеленский',
-  //   'Байдан',
-
-
-
-  // natsu['123 1000'] = 'asd';
-  // natsu['123 1000'] = 'sdknvkmlsmnfnfmff';
-  // natsu.remove('123 1000');
